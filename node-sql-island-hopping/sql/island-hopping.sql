5f009a72eb7f5fe92ee68dc14bc4658cb889823e
Kjenn180 <Kadeen.Jennings@gmail.com>
2023-01-10 21:05:39 -0500

    added continent table

--- node-sql-island-hopping/sql/island-hopping.sql
+++ node-sql-island-hopping/sql/island-hopping.sql
@@ -1,3 +1,11 @@
 create database island_hopping
 
 use island_hopping
+
+Create TABLE IF NOT EXISTS Continent (
+	continent_id INT auto_increment primary Key,
+    location VARCHAR(255) NOT NULL,
+    created_at timestamp DEFAULT current_timestamp,
+    updated_at timestamp DEFAULT current_timestamp
+) ENGINE=INNODB;
+
