d24053e8095f3fc0ec993360900ddb11e1102617
Kjenn180 <Kadeen.Jennings@gmail.com>
2023-01-10 21:40:26 -0500

    added country table

--- node-sql-island-hopping/sql/island-hopping.sql
+++ node-sql-island-hopping/sql/island-hopping.sql
@@ -9,3 +9,15 @@ Create TABLE IF NOT EXISTS Continent (
     updated_at timestamp DEFAULT current_timestamp
 ) ENGINE=INNODB;
 
+Create TABLE IF NOT EXISTS Country (
+	country_id INT auto_increment primary key,
+    location VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    created_at timestamp DEFAULT current_timestamp,
+    updated_at timestamp DEFAULT current_timestamp
+) ENGINE=INNODB;
+
+alter table Country add column continent_id int;
+alter table Country add foreign key(continent_id) references Continent(continent_id)
+
+    
