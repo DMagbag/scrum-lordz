871a86578424dddfd0649a4527a917c8bdfccf18
Kjenn180 <Kadeen.Jennings@gmail.com>
2023-01-13 22:37:42 -0500

    completed sql database

--- node-sql-island-hopping/sql/island-hopping.sql
+++ node-sql-island-hopping/sql/island-hopping.sql
@@ -52,76 +52,24 @@ update Country set continent_id = 3 where country_id = 4;
 update Country set continent_id = 3 where country_id = 5;
 update Country set continent_id = 2 where country_id = 6;
 
-insert into island_chain(location, adventures) values("Nassau","Home to the huge Atlantis resort 
-and its massive waterpark. Most beautiful 
-beaches in the world, indulging in world-class
-dining and nightlife, unwinding in award-
-winning spas, embarking on a thrilling 
-underwater adventure.");
-insert into island_chain(location, adventures) values("Harbour Island","Famous for its three-mile-long pink beach 
-that runs the entire length of the island on its
-eastern side. The beach is protected by an 
-outlying coral reef that makes the turquoise 
-clear water one of the safest and most 
-alluring swimming and snorkeling spots in 
-the Bahamas.");
-insert into island_chain(location, adventures) values("Exuma","Known for its many “elusive” 
-beaches, including world-famous Lighthouse 
-Beach, Twin Coves Beach, Double Bay 
-Beach, French Leave Beach and so many 
-others.");
-insert into island_chain(location, adventures) values("Pandanon Island","While Pandanon Island isn’t the best spot for diving and snorkeling, tourists take delight in
-swimming on the clear, shallow waters of the island. Its powdery white sand is best for
-frolicking by the beach or sunbathing.");
+insert into island_chain(location, adventures) values("Nassau","Home to the huge Atlantis resort and its massive waterpark. Most beautiful beaches in the world, indulging in world-classdining and nightlife, unwinding in award-winning spas, embarking on a thrilling underwater adventure.");
+insert into island_chain(location, adventures) values("Harbour Island","Famous for its three-mile-long pink beach that runs the entire length of the island on itseastern side. The beach is protected by an outlying coral reef that makes the turquoise clear water one of the safest and most alluring swimming and snorkeling spots in the Bahamas.");
+insert into island_chain(location, adventures) values("Exuma","Known for its many “elusive” beaches, including world-famous Lighthouse Beach, Twin Coves Beach, Double Bay Beach, French Leave Beach and so many others.");
+insert into island_chain(location, adventures) values("Pandanon Island","While Pandanon Island isn’t the best spot for diving and snorkeling, tourists take delight inswimming on the clear, shallow waters of the island. Its powdery white sand is best forfrolicking by the beach or sunbathing.");
 insert into island_chain(location, adventures) values("Olango Island","The best time to go to Olango Island, especially if you're planning to visit the Wildlife Sanctuary, is in November when birds are flying south for the breeding season.");
 insert into island_chain(location, adventures) values("Nalusuan Island","The long sand bar of Nalusuan island is a sanctuary to all the  marine creatures and to the nature lovers. You can literally reach at arm’s length with the abundance of sea life here. The fishes that you have only seen in your plates will be see here in their natural habitat.You will feel like you are a part of the sea, the island and the life under water.");
-insert into island_chain(location, adventures) values("Koh Tao","The island is well known 
-for scuba diving and snorkeling, as well as 
-hiking, rock climbing, and bouldering.");
-insert into island_chain(location, adventures) values("Koh Pha Ngan","An island that's world-
-famous for its beach parties, the iconic full-
-moon and half-moon parties.");
-insert into island_chain(location, adventures) values("Koh Samui","A popular resort island in 
-the Gulf of Thailand with stunning beaches 
-and plenty of fun activities.");
-insert into island_chain(location, adventures) values("Odaiba","A man-made island and 
-entertainment district, home to plenty of 
-shopping and dining.");
-insert into island_chain(location, adventures) values("Ogasawara Island","Famous for the 
-scale of wildlife in the waters around the 
-island, including whales and dolphins. ");
-insert into island_chain(location, adventures) values("Yakushima Island"," Famous for 
-dense cedar forests and incredible hiking.");
-insert into island_chain(location, adventures) values("Hvar","Rich history, superb cuisine, 
-diverse nightlife and breath-taking 
-nature are the reasons why both global 
-celebrities and those in search of unique 
-holiday locations choose Hvar");
-insert into island_chain(location, adventures) values("Dubrovnik","A captivating place to spend a 
-few days, with medieval fortifications, 
-limestone streets, seafood restaurants, and 
-rocky beaches to explore. However, there's a
-selection of unique adventures for those who
-want to get off the tourist trail.");
-insert into island_chain(location, adventures) values("Split","Known for its ancient centre, 
-beautiful architecture, and exquisite 
-cuisine. Whether you wish to admire 
-impressive Roman monuments, spend the 
-day on a picturesque beach with crystal-
-clear waters, or walk along one of the most
-beautiful promenades on the Adriatic 
-Coast.");
-insert into island_chain(location, adventures) values("Paros"," Known for its great 
-beaches, lively nightlife, and wonderful, 
-white-washed towns.");
-insert into island_chain(location, adventures) values("Mykonos","A smaller Greek island but offers
-big excitement for tourist in its culture, 
-restaurants, music, and nightlife");
-insert into island_chain(location, adventures) values("Santorini","Renowned for its tradition in 
-wine making and boasts vast vineyards and 
-fine wine varieties. Its boutique wineries are 
-a must as they offer the ultimate ambrosial 
-experience.");
+insert into island_chain(location, adventures) values("Koh Tao","The island is well known for scuba diving and snorkeling, as well as hiking, rock climbing, and bouldering.");
+insert into island_chain(location, adventures) values("Koh Pha Ngan","An island that's world-famous for its beach parties, the iconic full-moon and half-moon parties.");
+insert into island_chain(location, adventures) values("Koh Samui","A popular resort island in the Gulf of Thailand with stunning beaches and plenty of fun activities.");
+insert into island_chain(location, adventures) values("Odaiba","A man-made island and entertainment district, home to plenty of shopping and dining.");
+insert into island_chain(location, adventures) values("Ogasawara Island","Famous for the scale of wildlife in the waters around the island, including whales and dolphins. ");
+insert into island_chain(location, adventures) values("Yakushima Island"," Famous for dense cedar forests and incredible hiking.");
+insert into island_chain(location, adventures) values("Hvar","Rich history, superb cuisine, diverse nightlife and breath-taking nature are the reasons why both global celebrities and those in search of unique holiday locations choose Hvar");
+insert into island_chain(location, adventures) values("Dubrovnik","A captivating place to spend a few days, with medieval fortifications, limestone streets, seafood restaurants, and rocky beaches to explore. However, there's aselection of unique adventures for those whowant to get off the tourist trail.");
+insert into island_chain(location, adventures) values("Split","Known for its ancient centre, beautiful architecture, and exquisite cuisine. Whether you wish to admire impressive Roman monuments, spend the day on a picturesque beach with crystal-clear waters, or walk along one of the mostbeautiful promenades on the Adriatic Coast.");
+insert into island_chain(location, adventures) values("Paros"," Known for its great beaches, lively nightlife, and wonderful, white-washed towns.");
+insert into island_chain(location, adventures) values("Mykonos","A smaller Greek island but offersbig excitement for tourist in its culture, restaurants, music, and nightlife");
+insert into island_chain(location, adventures) values("Santorini","Renowned for its tradition in wine making and boasts vast vineyards and fine wine varieties. Its boutique wineries are a must as they offer the ultimate ambrosial experience.");
 
 select * from island_chain;
 
@@ -150,3 +98,102 @@ Create TABLE IF NOT EXISTS waterway (
     created_at timestamp DEFAULT current_timestamp,
     updated_at timestamp DEFAULT current_timestamp
 )ENGINE=INNODB;
+
+insert into waterway(location) values("Artic Ocean");
+insert into waterway(location) values("Pacific Ocean");
+insert into waterway(location) values("Indian Ocean");
+insert into waterway(location) values("Atlantic Ocean");
+insert into waterway(location) values("Gulf of Thailand");
+insert into waterway(location) values("Tokyo Bay");
+insert into waterway(location) values("North-Western Pacific Ocean");
+insert into waterway(location) values("East China Sea");
+insert into waterway(location) values("Adriatic Sea");
+insert into waterway(location) values("Aegean Sea");
+insert into waterway(location) values("Southern Aegean Sea");
+insert into waterway(location) values("Camotes Sea");
+insert into waterway(location) values("Philippine Sea");
+
+select * from waterway;
+
+create TABLE IF NOT EXISTS continent_waterways (
+	continent_waterway_id INT auto_increment primary KEY,
+    continent_id int,
+    waterway_id int,
+    created_at timestamp DEFAULT current_timestamp,
+    updated_at timestamp DEFAULT current_timestamp,
+    foreign key (continent_id) references continent(continent_id) on update cascade on delete cascade,
+    foreign key (waterway_id) references waterway(waterway_id) on update cascade on delete cascade
+) ENGINE=INNODB;
+
+insert into continent_waterways(continent_id, waterway_id) values(1,1);
+insert into continent_waterways(continent_id, waterway_id) values(1,2);
+insert into continent_waterways(continent_id, waterway_id) values(1,4);
+insert into continent_waterways(continent_id, waterway_id) values(2,1);
+insert into continent_waterways(continent_id, waterway_id) values(2,2);
+insert into continent_waterways(continent_id, waterway_id) values(2,3);
+insert into continent_waterways(continent_id, waterway_id) values(3,1);
+insert into continent_waterways(continent_id, waterway_id) values(3,4);
+
+select * from continent_waterways;
+
+create TABLE IF NOT EXISTS country_waterways (
+	country_waterway_id INT auto_increment primary KEY,
+    country_id int,
+    waterway_id int,
+    created_at timestamp DEFAULT current_timestamp,
+    updated_at timestamp DEFAULT current_timestamp,
+    foreign key (country_id) references country(country_id) on update cascade on delete cascade,
+    foreign key (waterway_id) references waterway(waterway_id) on update cascade on delete cascade
+) ENGINE=INNODB;
+
+insert into country_waterways(country_id, waterway_id) values(1,4);
+insert into country_waterways(country_id, waterway_id) values(2,3);
+insert into country_waterways(country_id, waterway_id) values(3,4);
+insert into country_waterways(country_id, waterway_id) values(4,4);
+insert into country_waterways(country_id, waterway_id) values(5,4);
+insert into country_waterways(country_id, waterway_id) values(6,2);
+
+select * from country_waterways; 
+
+create TABLE IF NOT EXISTS island_waterways (
+	island_waterway_id INT auto_increment primary KEY,
+    island_id int,
+    waterway_id int,
+    created_at timestamp DEFAULT current_timestamp,
+    updated_at timestamp DEFAULT current_timestamp,
+    foreign key (island_id) references island_chain(island_id) on update cascade on delete cascade,
+    foreign key (waterway_id) references waterway(waterway_id) on update cascade on delete cascade
+) ENGINE=INNODB;
+
+insert into island_waterways(island_id, waterway_id) values(1,4);
+insert into island_waterways(island_id, waterway_id) values(2,4);
+insert into island_waterways(island_id, waterway_id) values(3,4);
+insert into island_waterways(island_id, waterway_id) values(4,12);
+insert into island_waterways(island_id, waterway_id) values(5,12);
+insert into island_waterways(island_id, waterway_id) values(6,13);
+insert into island_waterways(island_id, waterway_id) values(7,5);
+insert into island_waterways(island_id, waterway_id) values(8,5);
+insert into island_waterways(island_id, waterway_id) values(9,5);
+insert into island_waterways(island_id, waterway_id) values(10,6);
+insert into island_waterways(island_id, waterway_id) values(11,7);
+insert into island_waterways(island_id, waterway_id) values(12,8);
+insert into island_waterways(island_id, waterway_id) values(13,9);
+insert into island_waterways(island_id, waterway_id) values(14,9);
+insert into island_waterways(island_id, waterway_id) values(15,9);
+insert into island_waterways(island_id, waterway_id) values(16,10);
+insert into island_waterways(island_id, waterway_id) values(17,10);
+insert into island_waterways(island_id, waterway_id) values(18,11);
+
+select * from island_waterways;
+
+select country.country_id, country.location
+from country
+join country_waterways on country_waterways.waterway_id = 3 and country_waterways.waterway_id = country.country_id;
+
+select continent.location, country.country_id, country.location
+from continent, country
+where continent.continent_id = country.continent_id and continent.location = 'Asia';
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'wccibootcamp';
+
+flush privileges;
