aaa4105d9ddfb00f2d5f0b03fa7d41ae97abb0d2
Kjenn180 <Kadeen.Jennings@gmail.com>
2023-01-13 23:56:11 -0500

    added functions to both island-hopping.js and app.js

---
+++ node-sql-island-hopping/routes/island-hopping.js
@@ -0,0 +1,20 @@
+let express = require("express")
+let campusRouter = express.Router();
+const dbConn = require();
+
+//display continents on the page
+continentRouter.get("/", function (req, res, next){
+    return new Promise((resolve, reject) => {
+        dbConn.query(
+            "SELECT * FROM continent ORDER BY continent_id desc",
+            function (err, rows) {
+                if (err) {
+                    req.flash("error", err);
+                    // render to views/campuses/index.ejs
+                    res.render("campuses", {data: rows});
+                }
+            }
+        )
+    });
+});
+module.exports = continentRouter;
